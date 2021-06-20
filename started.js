efec86f4032f0e5e7d918b42a1448f8883f7b788
Resaw-git <resavv@mail.ru>
2021-06-21 01:56:42 +0300

    complite background and content area

--- started.js
+++ started.js
@@ -1,1 +1,27 @@
-document.writeln("123");
+var content = document.querySelector(".content");
+var headerImg = new Image(0, 200);
+headerImg.src = 'images/dota2.jpg';
+headerImg.width = document.documentElement.clientWidth * 0.75;
+content.innerHTML = headerImg.outerHTML;
+
+function resizeWidthOnly(a,b) {  
+    var c = [window.innerWidth];
+    return onresize = function() {
+      var d = window.innerWidth,
+          e = c.length;
+      c.push(d);
+      if(c[e]!==c[e-1]){
+        clearTimeout(b);
+        b = setTimeout(a, 10);
+      } 
+    }, a;
+  }
+
+  resizeWidthOnly(function() {
+    headerImg.width = document.documentElement.clientWidth * 0.75;
+    content.innerHTML = headerImg.outerHTML;
+  });
+
+var headerBar = document.createElement('div');
+headerBar.innerHTML = "Dota 2";
+content.appendChild(headerBar);
